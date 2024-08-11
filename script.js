5476c136a7f259da05d17afea5031b3850234c2c
Code-With-Vijeta <161485670+Code-With-Vijeta@users.noreply.github.com>
2024-08-11 14:14:28 +0530

    initial file added

---
+++ script.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset: true ,
+    distance:"80px" ,
+    duration:2000,
+    delay:200
+   });
+ 
+   ScrollReveal().reveal('.wishes_content ', { origin: 'left' });
+   ScrollReveal().reveal('.wishes', { origin: 'right' });
